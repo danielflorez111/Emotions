f7c82e5b7c065be34ffda9bc65dc4e56519e2b1c
Daniel Felipe Florez Salgado <danielflorez111@hotmail.com>
2017-08-28 11:09:15 -0500

    Util

---
+++ EmotionPlatzi.Web/Util/EmotionHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Common.Contract;
+using EmotionPlatzi.Web.Models;
+using System.Collections.ObjectModel;
+
+namespace EmotionPlatzi.Web.Util
+{
+    public class EmotionHelper
+    {
+        public EmotionServiceClient emoClient;
+
+        public EmotionHelper(string key)
+        {
+            emoClient = new EmotionServiceClient(key);
+        }
+
+        public async void DetectAndExtractFaces(Stream imageStream)
+        {
+            Emotion[] emotions = await emoClient.RecognizeAsync(imageStream);
+
+            var emoPicture = new EmoPicture();
+
+            emoPicture.Faces = ExtractFaces(emotions, emoPicture);
+        }
+
+        private ObservableCollection<EmoFace> ExtractFaces(Emotion[] emotions, EmoPicture emoPicture)
+        {
+            emoPicture.Faces = new ObservableCollection<EmoFace>();
+
+            foreach (var emotion in emotions)
+            {
+                emoPicture.Faces.Add(new EmoFace()
+                {
+                    X = emotion.FaceRectangle.Left,
+                    Y = emotion.FaceRectangle.Top,
+                    Width = emotion.FaceRectangle.Width,
+                    Height = emotion.FaceRectangle.Height,
+                    Picture = emoPicture
+                });
+            }
+        }
+    }
+}
